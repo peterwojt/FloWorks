dd0bd075ef6080882b07ad54113c7ce8ccba3fbb
user <156293185+peterwojt@users.noreply.github.com>
2026-06-10 17:43:38 -0700

    Added backend updating of cursor position and acceleration across many clients using a WebSocket connection.

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,66 @@
 const express = require('express');
+const { WebSocketServer, WebSocket } = require('ws');
 const app = express();
-const port = 8080;
+const http = require('http');
+const PORT = 8080;
+const path = require('path');
 
-// Define a route for GET requests to the root URL
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ 
+  server: server,
+  path: '/update_cursor_position' 
+});
+
+var number_of_connected_clients = 0;
+
+function update_cursor_position(x_pos, y_pos, x_vector, y_vector) {
+
+  const messageData = {
+    x_pos: x_pos,
+    y_pos: y_pos,
+    x_vector: x_vector,
+    y_vector: y_vector
+  };
+
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(messageData));
+    }
+  });
+}
+
+
+app.use(express.static(path.join(__dirname, '.')));
+
 app.get('/', (req, res) => {
-  res.send('Hello World from Express!');
+  res.sendFile(path.join(__dirname, '.', 'test.html'));
+});
+
+wss.on('connection', (ws) => {
+  number_of_connected_clients += 1;
+
+  ws.on('message', (message) => {
+    const data = JSON.parse(message);
+    const x_pos = data.x_pos;
+    const y_pos = data.y_pos;
+    const x_vector = data.x_vector;
+    const y_vector = data.y_vector;
+
+    update_cursor_position(x_pos, y_pos, x_vector, y_vector);
+  });
+
+  ws.on('close', () => {
+    number_of_connected_clients -= 1;
+  });
+});
+
+
+app.get('/api/number_of_connected_clients', (req, res) => {
+  return res.json({ number_of_connected_clients: number_of_connected_clients });
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-}); 
+server.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
